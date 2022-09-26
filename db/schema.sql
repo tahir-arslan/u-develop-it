fca75feea3d4f94fc1a11854b960d71e8e8f2565
ThisOneGuy <tahir.arslan@gmail.com>
2022-09-26 16:43:23 -0400

    finished module 12.3

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,28 @@
+-- drop tables if exist to ensure working with the most to date tables
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+-- parties table
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
+-- candidates table
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    -- constraint allows us to flag foreign key and tell QL which table and field it references
+        -- since constraint relies on parties table, parties table MUST be defined first before
+        -- candidates table (therefore the list sequence in this file)
+    -- since strict rule established that no candidate can be a member of a party that doesn't
+    -- exist, then if party is deleted `ON DELETE SET NULL`
+        -- NOTE: because of the dependancy of the foreign key, candidates table must also be
+        -- dropped before parties table as well since foreign key constraint requires the parties
+        -- table to exist
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
+// file provided by lesson
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// file provided by lesson
-const inputCheck = require('./utils/inputCheck');
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -17,7 +16,7 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         // change password to match login for MySQL
-        password: 'password',
+        password: ' ',
         database: 'election'
     },
     console.log('Connected to the election database.')
@@ -25,7 +24,9 @@ const db = mysql.createConnection(
 
 // return single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates
+                        LEFT JOIN partiies ON candidates.party_id = parties.id
+                        WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.query(sql, params, (err, row) => {
         if (err) {
@@ -41,7 +42,8 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // return all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates
+                        LEFT JOIN parties ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
@@ -78,6 +80,34 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+// update candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    // this ensures that 'party_id' is being used for any PUT request to '/api/candidates/:id'
+    const errors = inputCheck(req.body, 'party_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    };
+    const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+    // to follow best practice: affected row id should always be part of route (ex `/api/candidate/2`)
+    // while actual fields being updated should be part of body
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if record exists
+        } else if (!result.affectedRows) { 
+            res.json({ message: 'Candidate not found.' }); 
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 // delete candidate
 // `?` is a placeholder, making this a prepared statement (can execute same SQL statement
 // repeatedly using different values in place of placeholder)
@@ -91,6 +121,57 @@ app.delete('/api/candidate/:id', (req, res) => {
             res.json({ message: 'Candidate not found'}) ;
         } else {
             res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// route for single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// route for all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// delete party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({ message: 'Party not found.' });
+        } else {
+            res.json ({
                 message: 'deleted',
                 changes: result.affectedRows,
                 id: req.params.id
